1cabfe7ec0c6a0b3ddf06e332770f8d5140658a8
Michael Keen <mjkeen@bsu.edu>
2020-09-08 11:10:40 -0400

    Added math methods subtraction, multiplication, and division. Also added tests for each method (including division by zero)

--- src/main/java/com/team9/CalculatorController.java
+++ src/main/java/com/team9/CalculatorController.java
@@ -4,5 +4,21 @@ public class CalculatorController {
 
     public int add(int number, int secondNumber) {
         return number + secondNumber;
-    };
+    }
+    public int subtract(int number, int secondNumber) {
+        return number - secondNumber;
+    }
+
+    public int multiply(int number, int secondNumber) {
+        return number * secondNumber;
+    }
+
+    public int divide(int number, int secondNumber) {
+        if (secondNumber == 0) {
+            return 0; //can't divide by zero
+        } else {
+            return number / secondNumber;
+        }
+    }
+
 }
