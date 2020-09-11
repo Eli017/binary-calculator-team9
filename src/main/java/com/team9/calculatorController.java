8d7e22d8b671dbe29210ca8dde7d24baa4808a27
Duan Rollins <drollins@bsu.edu>
2020-09-11 16:22:07 -0400

    completed: .button 0 .button 1 .started all math functions(stores the values but doesn't perform the math) .clear is working

--- src/main/java/com/team9/calculatorController.java
+++ src/main/java/com/team9/calculatorController.java
@@ -3,8 +3,13 @@ package com.team9;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
+import java.math.MathContext;
+
 
 public class calculatorController{
+    @FXML
+    TextField numberField;
+    public Boolean isButtonPressed = false;
 
     @FXML
     private void toggleBinaryArabic() {
@@ -16,64 +21,100 @@ public class calculatorController{
         //TODO: implement Calculator interface to convert string input to logical number.
     }
 
-    @FXML
-    public void currentNumber() {
-        TextField NumberField;
-    }
-    //Need help unsuccessful changes
-    @FXML
-    public TextField numberField;
 
     @FXML
     private void enterZero() {
-            String numberField = "0";
-        //TODO: append string with 0.
+        if (numberField.getText().equals("0")) {
+            System.out.println("It is already equal to 0.");
+        } else {
+            numberField.setText(numberField.getText().concat("0"));
+        }
     }
 
     @FXML
     private void enterOne() {
-        String numberField = "1";
-        //TODO: append string with 1.
+        if (numberField.getText().equals("0")) {
+            numberField.setText("1");
+            }
+        else {
+            numberField.setText(numberField.getText().concat("1"));
+        }
     }
 
     @FXML
     private void addClicked() {
-        //TODO: store first number and work logic around next number.
+        //TODO: logic around next number.
+        if (isButtonPressed.equals(true)) {
+            System.out.println("Must hit = or change second number value");
+        } else {
+            numberField.setText(numberField.getText().concat(" + "));
+            isButtonPressed = true;
+        }
     }
 
     @FXML
     private void subtractClicked() {
-        //TODO: store first number and work logic around next number.
+        //TODO: logic around next number.
+        if (isButtonPressed.equals(true)) {
+            System.out.println("Must hit = or change second number value");
+        }else {
+            numberField.setText(numberField.getText().concat(" - "));
+            isButtonPressed = true;
+        }
     }
 
     @FXML
     private void multiplyClicked() {
-        //TODO: store first number and work logic around next number.
+        //TODO: logic around next number.
+        if (isButtonPressed.equals(true)) {
+            System.out.println("Must hit = or change second number value");
+        }else {
+            numberField.setText(numberField.getText().concat(" * "));
+            isButtonPressed = true;
+        }
     }
 
     @FXML
     private void divideClicked() {
-        //TODO: store first number and work logic around next number.
+        //TODO: logic around next number.
+        if (isButtonPressed.equals(true)) {
+            System.out.println("Must hit = or change second number value");
+        }else {
+            numberField.setText(numberField.getText().concat(" / "));
+            isButtonPressed = true;
+        }
     }
 
     @FXML
     private void squareClicked() {
-        //TODO: store first number and work logic around next number.
-    }
-
-    @FXML
-    private void equalClicked() {
-        //TODO: Perform math to add two binary numbers and populate the result area.
+        //TODO: logic around next number.
+        if (isButtonPressed.equals(true)) {
+            System.out.println("Must hit = or change second number value");
+        }else {
+            isButtonPressed = true;
+        }
     }
 
     @FXML
     private void sqrtClicked() {
         //TODO: Perform sqrt math for current number and populate result area.
+        if (isButtonPressed.equals(true)) {
+            System.out.println("Must hit = or change second number value");
+        }else {
+            isButtonPressed = true;
+        }
     }
 
     @FXML
     private void clearClicked() {
+        numberField.setText("0");
         //TODO: Clear variables and TextField area input.
+        isButtonPressed = false;
+    }
 
+    @FXML
+    private void equalClicked() {
+        //TODO: Perform math to add two binary numbers and populate the result area.
+        numberField.getText();
     }
 }
